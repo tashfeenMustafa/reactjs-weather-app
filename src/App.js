c25c27d1d1dc8b3c6f06bd82dec91fc3dabcda7d
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2021-06-10 04:00:27 +0600

    Added functionality to get user location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h1>ReactJS Weather App</h1>
-      </header>
-    </div>
-  );
+let getLocationSuccess = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  console.log(latitude, longitude);
+}
+
+let getLocationError = (error) => {
+  console.log(error);
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      location: {
+        latitude: 0,
+        longitude: 0,
+      }
+    }
+  }
+  componentDidMount () {
+    const deviceLocation = navigator.geolocation;
+
+    if (!deviceLocation) {
+      window.alert('GeoLocation is not supported on your device.');
+    }
+    else {
+      deviceLocation.getCurrentPosition(getLocationSuccess, getLocationError);
+    }
+  }
+  render () {
+    return (
+      <div>
+        <header className="header">
+          <h1>ReactJS Weather App</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
